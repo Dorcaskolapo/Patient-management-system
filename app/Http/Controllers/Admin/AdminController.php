bc09a1efe7e717012381f3ab40f26c71b98900b4
skyhackeR <jolayemidavid6@gmail.com>
2024-03-15 15:58:01 +0100

    Adding of admin pages

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -11,12 +11,29 @@ use Illuminate\Support\Facades\Hash;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Validator;
 
+use App\Models\Billing;
+use App\Models\Drug;
+use App\Models\Admin;
+use App\Models\Patient;
+use App\Models\Prescription;
+use App\Models\Role;
+use App\Models\Staff;
+use App\Models\Test;
+
+
 class AdminController extends Controller
 {
     //
 
     public function index(){
 
-        return view('admin.home');
+        return view('admin.dashboard');
+    }
+
+    public function staff(){
+        $staff = Staff::get();
+        return view('admin.staff', [
+            'staff' => $staff
+        ]);
     }
 }
