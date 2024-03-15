ebc51dca3239e26243ef0e13b52ede924893a396
skyhackeR <jolayemidavid6@gmail.com>
2024-03-15 10:40:29 +0100

    Staff routing to homepage

--- app/Http/Controllers/Staff/StaffController.php
+++ app/Http/Controllers/Staff/StaffController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\Staff;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
 
 class StaffController extends Controller
 {
     //
+    public function index(){
+
+        return view('staff.home');
+    }
 }
