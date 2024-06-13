137bc28b42bca83fd8409beae7bd76400888214d
dorcaskolapo5@gmail.com <dorcaskolapo5@gmail.com>
2024-06-13 10:11:44 +0100

    generating of resources/view

--- resources/views/admin/allPatient.blade.php
+++ resources/views/admin/allPatient.blade.php
@@ -232,7 +232,7 @@
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary"
                                         data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>""
                                 </div>
                             </div>
                         </div>
@@ -243,5 +243,6 @@
     </div>
     <!-- End section content -->
 
+    
 
    
